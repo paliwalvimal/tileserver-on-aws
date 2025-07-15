0f52fa91b80fe3ebf0a2921ed4b44844031754de
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2025-07-15 11:19:50 +0000

    use ruff for python

--- tileserver_apigw_lambda_authorizer.py
+++ tileserver_apigw_lambda_authorizer.py
@@ -1,54 +1,63 @@
-'''
-Lambda authorizer for tileserver http api gateway. Check performed:
-- Validates the custom header value received from cloudfront against the
-  value stored in AWS SSM parameter store
-'''
+"""Lambda authorizer for tileserver http api gateway.
 
-import os
+Check performed:
+- Validates the custom header value received from cloudfront against the
+  value stored in AWS SSM parameter store.
+"""
+
 import logging
+import os
+
 import boto3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME = os.environ.get("TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME", None)
-TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME = os.environ.get("TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME", None)
+TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME = os.environ.get(
+    "TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME", None
+)
+TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME = os.environ.get(
+    "TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME", None
+)
 REGION_NAME = os.environ.get("AWS_REGION", None)
 
 ssm = boto3.client("ssm", region_name=REGION_NAME)
 
+
 def fetch_ssm_param(parameter_name: str) -> str:
-    '''
-    Get the parameter value from AWS SSM
+    """Get the parameter value from AWS SSM.
 
     Args:
         parameter_name: Name of the SSM parameter to fetch
 
     Returns:
         str: Value of the SSM parameter
-    '''
+
+    """
     logger.info("Getting parameter value from SSM: %s", parameter_name)
-    response = ssm.get_parameter(
-        Name=parameter_name,
-        WithDecryption=True
-    )
+    response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
     return response["Parameter"]["Value"]
 
+
 def is_authorized(event: dict) -> bool:
-    '''
-    Authorize the request based on the custom header value received from cloudfront
+    """Authorize the request based on the custom header value received from cloudfront.
 
     Args:
         event: Event received from the API Gateway
 
     Returns:
         bool: True if the request is authorized, False otherwise
-    '''
+
+    """
     # validating the custom authz token
     try:
         logger.info("Validating the custom authz token...")
         authz_token_value_from_ssm = fetch_ssm_param(TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME)
-        authz_token_value_from_req_header = [v for k, v in event["headers"].items() if k.lower() == TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME.lower()][0]
+        authz_token_value_from_req_header = next(
+            v
+            for k, v in event["headers"].items()
+            if k.lower() == TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME.lower()
+        )
 
         if authz_token_value_from_ssm != authz_token_value_from_req_header:
             logger.error("Custom authz token mismatch")
@@ -56,43 +65,48 @@ def is_authorized(event: dict) -> bool:
 
         logger.info("Custom authz token match")
     except IndexError:
-        logger.error("Custom authz token not found in the request headers")
+        logger.exception("Custom authz token not found in the request headers")
         return False
-    except Exception as ex:
-        logger.error("Error validating the custom authz token. Reason: %s", ex)
+    except Exception:
+        logger.exception("Error validating the custom authz token")
         return False
 
     return True
 
+
 def validate_request(event: dict) -> bool:
-    '''
-    Validate the incoming request
+    """Validate the incoming request.
 
     Args:
         event: Event received from the API Gateway
 
     Returns:
         bool: True if the request is valid, False otherwise
-    '''
 
-    if (TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME is None
+    """
+    if (
+        TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME is None
         or TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME is None
     ):
-        logger.error("TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME or \
-                      TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME is not set")
+        logger.error(
+            "TILESERVER_AUTHZ_CF_TOKEN_CUSTOM_HEADER_NAME or \
+                      TILESERVER_AUTHZ_CF_TOKEN_SSM_PARAM_NAME is not set",
+        )
         return False
 
-    if ("headers" in event and "host" in event["headers"] and
-        event["headers"]["host"].endswith(f".execute-api.{REGION_NAME}.amazonaws.com")
+    if (
+        "headers" in event
+        and "host" in event["headers"]
+        and event["headers"]["host"].endswith(f".execute-api.{REGION_NAME}.amazonaws.com")
     ):
         logger.error("Direct access via API gateway is not allowed")
         return False
 
     return True
 
+
 def handler(event: dict, context: dict) -> dict:
-    '''
-    Entry point for the lambda function
+    """Entry point for the lambda function.
 
     Args:
         event: Event received from the API Gateway
@@ -100,13 +114,13 @@ def handler(event: dict, context: dict) -> dict:
 
     Returns:
         dict: Response from the lambda function
-    '''
 
+    """
     if not validate_request(event):
         return {
-            "isAuthorized": False
+            "isAuthorized": False,
         }
 
     return {
-        "isAuthorized": is_authorized(event)
+        "isAuthorized": is_authorized(event),
     }
